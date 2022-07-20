45efd34e18600888b136f6c411ff6b4d391f6c85
huynv184120 <nguyenviethuy2403@gmail.com>
2022-07-21 00:24:04 +0700

    update

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const socketEvent = require("./src/socket/events");
 dotenv.config();
 
 const configCors = {
-    origin: ["https://chat-client-itss.herokuapp.com","https://3000-huynv184120-itssjapanes-g8g7re716a3.ws-us54.gitpod.io"],
+    origin: ["https://chat-client-itss.herokuapp.com"," https://matching-app-itss.herokuapp.com"],
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials:true,
     optionsSuccessStatus: 204
@@ -20,7 +20,7 @@ const configCors = {
 
 const configCorsSocket = {
     cors: {
-      origin: ["https://chat-client-itss.herokuapp.com","https://3000-huynv184120-itssjapanes-g8g7re716a3.ws-us54.gitpod.io"],
+      origin: ["https://chat-client-itss.herokuapp.com"," https://matching-app-itss.herokuapp.com"],
       credentials:true,
       allowedHeaders: ['Content-Type', 'Authorization', 'Accept', 'Origin', 'Cookies']    
     }
